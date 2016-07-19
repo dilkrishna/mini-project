d95cdf80e3b20dd3e5912fb34d69a044fb472a55
pila <pila_dil@yahoo.com>
2016-07-19 17:35:45 +0545

    added model to login

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -15,13 +15,20 @@ class LoginController extends MY_Controller{
         $this->form_validation->set_error_delimiters('<P class= "text-danger">','</p>');
         
         if($this->form_validation->run()){
-            echo "sucessful";
-//        $username = $this->input->post('username');
-//        $password =  hash('sha512', $this->input->post('password'));
-//        echo $username;
+        $username = $this->input->post('username');
+        $password =  hash('sha512', $this->input->post('password'));
+        
+        $this->load->model('login_model');
+        $login = $this->login_model->login_valid($username,$password);
+            if($login){
+                echo "successful log in";
+            }
+            else{
+                echo 'invalid username and password';
+        }
         }
         else{
-            $this->load->view('public/admin_login');
+              $this->load->view('public/admin_login');
         }
 }
 }
---
+++ application/models/login_model.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class login_model extends CI_Model{
+    
+    public function login_valid($username,$password)
+    {
+       $q= $this->db->where(['username'=>$username, 'password'=>$password])
+                    ->get('user_table');
+          if( $q->row()){
+              $q->row()->id;
+          return TRUE;
+          }
+          
+          else{
+              return FALSE;
+          }
+    }
+}
