ac1767e159fb7fc3a8144cebb9d18b8ba495099c
pila <pila_dil@yahoo.com>
2016-07-19 12:27:01 +0545

    login form validation

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -3,8 +3,24 @@
 
 class LoginController extends MY_Controller{
 	
-public function index()
+    public function index()
     {
-    $this->load->view('public/admin_login');
+        $this->load->view('public/admin_login');
     }
+    
+    public function admin_login()
+    {
+        $this->form_validation->set_rules('username','username','required|alpha|trim|');
+        $this->form_validation->set_rules('password','password','required|');
+        
+        if($this->form_validation->run()){
+            echo "sucessful";
+//        $username = $this->input->post('username');
+//        $password =  hash('sha512', $this->input->post('password'));
+//        echo $username;
+        }
+        else{
+            $this->load->view('public/admin_login');
+        }
+}
 }
--- application/views/public/admin_login.php
+++ application/views/public/admin_login.php
@@ -1,30 +1,45 @@
 <?php include_once 'public_header.php'?>
 <div class="container">
-<?php echo form_open('AdminController/dashboard',['class'=>'form-horizontal']); ?>
+<?php echo form_open('LoginController/admin_login',['class'=>'form-horizontal']); ?>
 <!--<form class="form-horizontal" action="AdminController/dashboard">-->
   <fieldset>
     <legend>Article</legend>
-    <div class="form-group">
-        <label for="username" class="col-lg-2 control-label">Username</label>
-        <div class="col-lg-10">
-            <?php echo form_input(['class'=>'form-control','placeholder'=>'Username','type'=>'text'])?>
-<!--            <input class="form-control" placeholder="Username" type="text" >-->
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="form-group">
+                    <label for="username" class="col-lg-2 control-label">Username</label>
+                    <div class="col-lg-6">
+                        <?php echo form_input(['class'=>'form-control','placeholder'=>'Username','name'=>'username','type'=>'text','value'=>  set_value('username')])?>
+                <!--            <input class="form-control" placeholder="Username" type="text" >-->
+                   </div>
+                </div> 
+            </div>
+            <div class="col-lg-6">
+                <?php echo form_error('username','<p class=text-danger>','</p>'); ?>
+            </div>
+        </div> 
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="form-group">
+                <label for="password" class="col-lg-2 control-label">Password</label>
+                    <div class="col-lg-6">
+                        <?php echo form_input(['class'=>'form-control','placeholder'=>'Password','name'=>'password','type'=>'password']) ?>
+                        <!--<input class="form-control" placeholder="Password" type="password">-->
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <?php echo form_error('password','<p class=text-danger>','</P>');?>
+                </div>
+            </div>
         </div>
-    </div>    
-     <div class="form-group">
-        <label for="password" class="col-lg-2 control-label">Password</label>
-        <div class="col-lg-10">
-            <?php echo form_input(['class'=>'form-control','placeholder'=>'Password','type'=>'password']) ?>
-            <!--<input class="form-control" placeholder="Password" type="password">-->
-        </div>
-    </div>
     <div class="form-group">
       <div class="col-lg-10 col-lg-offset-2">
-        <button type="reset" class="btn btn-default">Cancel</button>
+        <button type="reset" class="btn btn-default">Reset</button>
         <button type="submit" class="btn btn-primary">Submit</button>
       </div>
     </div>
   </fieldset>
+</div>
 <?php echo form_close(); ?>
 </div>
 </fieldset>
